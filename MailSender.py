24a4633b674d4d87cea903fa4be0832ac8e1070a
Damidas0 <damien.laqueuvre@gmail.com>
2024-10-08 17:19:55 +0200

    mail

--- MailSender.py
+++ MailSender.py
@@ -2,28 +2,38 @@ import smtplib
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 
+
 # Fonction pour envoyer des emails
 def send_emails(assignments, workshops, smtp_server, smtp_port, sender_email):
     for email, assigned_workshops in assignments.items():
+        print("a")
         if not assigned_workshops:
             continue
         subject = "Attribution des Ateliers"
         body_start = input(f"Entrez le message de début pour {email}: ")
         body_end = input(f"Entrez le message de fin pour {email}: ")
 
-        workshop_names = [workshops[workshops['ID'] == wid]['Nom'].values[0] for wid in assigned_workshops]
-        workshop_details = "\n".join(workshop_names)
-
+        #workshop_names = [workshops[workshops['ID'] == wid]['Nom'].values[0] for wid in assigned_workshops]
+        #workshop_details = "\n".join(workshop_names)
+        workshop_details = "dizqjdqiojzqdiqzjoi"
         body = f"{body_start}\n\nLes ateliers suivants vous ont été attribués :\n{workshop_details}\n\n{body_end}"
-
+        print("a")
         msg = MIMEMultipart()
+        print("a")
         msg['From'] = sender_email
         msg['To'] = email
+        print("a")
         msg['Subject'] = subject
 
         msg.attach(MIMEText(body, 'plain'))
-
+        print("b")
         server = smtplib.SMTP(smtp_server, smtp_port)
+        print('c')
         server.starttls()
+        print("adjizdjqioz")
+        server.login(sender_email, 'Festival2024!')
+        #smtp_server.login('ambre.laqueuvre@gmail.com', 'PASSWORD')
+        print("c")
         server.send_message(msg)
+        print("d")
         server.quit()
--- main.py
+++ main.py
@@ -5,28 +5,35 @@ from MailSender import *
 
 # Main
 def main():
-    # Charger le fichier Excel
-    excel_file = load_excel_file()
+    ## Charger le fichier Excel
+    #excel_file = load_excel_file()
+    #
+#   # delete_attribution(excel_file)
+#
+    ## Lire les données des participants et des ateliers
+    #participants = read_participant_data(excel_file)
+    #print(participants)
+    #workshops = read_workshop_data(excel_file)
+    #print(workshops)
+#
+    ## Exécuter l'algorithme des mariages stables
+    #assignments, waitlist = stable_marriage(participants, workshops)
+#
+    ## Enregistrer les attributions dans une nouvelle feuille Excel
+    #save_assignments(excel_file, assignments, waitlist)
+
     
-#    delete_attribution(excel_file)
-
-    # Lire les données des participants et des ateliers
-    participants = read_participant_data(excel_file)
-    print(participants)
-    workshops = read_workshop_data(excel_file)
-    print(workshops)
-
-    # Exécuter l'algorithme des mariages stables
-    assignments, waitlist = stable_marriage(participants, workshops)
-
-    # Enregistrer les attributions dans une nouvelle feuille Excel
-    save_assignments(excel_file, assignments, waitlist)
-
+    
+    assignments = {'ambre.laqueuvre@gmail.com':'aaaaaaaaa'}
+    workshops = {'aaaaaaaaa':'blablabla'}
+    
     # Envoyer les emails
-    smtp_server = "smtp.example.com"  # Remplacer par le serveur SMTP réel
+    smtp_server = 'mail.mailo.com'
     smtp_port = 587
-    sender_email = "no-reply@example.com"  # Remplacer par l'adresse email réelle
-    #send_emails(assignments, workshops, smtp_server, smtp_port, sender_email)
+    
+ 
+    sender_email = "contact-ladouille@mailo.com"  # Remplacer par l'adresse email réelle
+    send_emails(assignments, workshops, smtp_server, smtp_port, sender_email)
 
 if __name__ == "__main__":
     main()
